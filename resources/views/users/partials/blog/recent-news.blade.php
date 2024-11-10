5b299ef81be9f754a0b1ae284716b43c78921806
Dat <0306221011@caothang.edu.vn>
2024-11-10 10:27:12 +0700

    update_pages_and_add_contact_home_folder

---
+++ resources/views/users/partials/blog/recent-news.blade.php
@@ -0,0 +1,30 @@
+<h4>Recent News</h4>
+<div class="blog__sidebar__recent">
+    <a href="#" class="blog__sidebar__recent__item">
+        <div class="blog__sidebar__recent__item__pic">
+            <img src="img/blog/sidebar/sr-1.jpg" alt="">
+        </div>
+        <div class="blog__sidebar__recent__item__text">
+            <h6>09 Kinds Of Vegetables<br /> Protect The Liver</h6>
+            <span>MAR 05, 2019</span>
+        </div>
+    </a>
+    <a href="#" class="blog__sidebar__recent__item">
+        <div class="blog__sidebar__recent__item__pic">
+            <img src="img/blog/sidebar/sr-2.jpg" alt="">
+        </div>
+        <div class="blog__sidebar__recent__item__text">
+            <h6>Tips You To Balance<br /> Nutrition Meal Day</h6>
+            <span>MAR 05, 2019</span>
+        </div>
+    </a>
+    <a href="#" class="blog__sidebar__recent__item">
+        <div class="blog__sidebar__recent__item__pic">
+            <img src="img/blog/sidebar/sr-3.jpg" alt="">
+        </div>
+        <div class="blog__sidebar__recent__item__text">
+            <h6>4 Principles Help You Lose <br />Weight With Vegetables</h6>
+            <span>MAR 05, 2019</span>
+        </div>
+    </a>
+</div>
---
+++ resources/views/users/partials/shoping-cart/shoping-btns.blade.php
@@ -0,0 +1,5 @@
+<div class="shoping__cart__btns">
+    <a href="#" class="primary-btn cart-btn">CONTINUE SHOPPING</a>
+    <a href="#" class="primary-btn cart-btn cart-btn-right"><span class="icon_loading"></span>
+        Upadate Cart</a>
+</div>

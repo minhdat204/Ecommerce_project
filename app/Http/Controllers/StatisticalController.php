87ea54917fb9fd0f1eb07da01be4fe2000c45f01
khoacute <0306221032@caothang.edu.vn>
2024-11-17 18:40:22 +0700

    xong layout admin + model-controller-router

---
+++ app/Http/Controllers/StatisticalController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Statistical;
+use Illuminate\Http\Request;
+
+class StatisticalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Statistical $statistical)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Statistical $statistical)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Statistical $statistical)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Statistical $statistical)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StatisticalController;
 
 Route::get('/', function () {
     return view('users.pages.home');
@@ -28,3 +35,77 @@ Route::get('/shoping-cart', function () {
 Route::get('/checkout', function () {
     return view('users.pages.checkout');
 })->name('users.checkout');
+// admin
+Route::get('/administrator', function () {
+    return view('admin.pages.category'); // giao diện mẫu = Category
+})->name('dashboard');
+
+Route::get('/mau', function () {
+    return view('admin.pages.category'); // giao diện mẫu = Category
+})->name('mau');
+
+
+
+Route::resource('dashboard', DashboardController::class)->names([
+    'index'   => '',
+    'create'  => '',
+    'store'   => '',
+    'show'    => '',
+    'edit'    => '',
+    'update'  => '',
+    'destroy' => '',
+]);
+Route::resource('categories', CategoryController::class)->names([
+    'index'   => '',
+    'create'  => '',
+    'store'   => '',
+    'show'    => '',
+    'edit'    => '',
+    'update'  => '',
+    'destroy' => '',
+]);
+Route::resource('comments', CommentController::class)->names([
+    'index'   => '',
+    'create'  => '',
+    'store'   => '',
+    'show'    => '',
+    'edit'    => '',
+    'update'  => '',
+    'destroy' => '',
+]);
+Route::resource('contacts', ContactController::class)->names([
+    'index'   => '',
+    'create'  => '',
+    'store'   => '',
+    'show'    => '',
+    'edit'    => '',
+    'update'  => '',
+    'destroy' => '',
+]);
+Route::resource('orders', OrderController::class)->names([
+    'index'   => '',
+    'create'  => '',
+    'store'   => '',
+    'show'    => '',
+    'edit'    => '',
+    'update'  => '',
+    'destroy' => '',
+]);
+Route::resource('products', ProductController::class)->names([
+    'index'   => '',
+    'create'  => '',
+    'store'   => '',
+    'show'    => '',
+    'edit'    => '',
+    'update'  => '',
+    'destroy' => '',
+]);
+Route::resource('statisticals', StatisticalController::class)->names([
+    'index'   => '',
+    'create'  => '',
+    'store'   => '',
+    'show'    => '',
+    'edit'    => '',
+    'update'  => '',
+    'destroy' => '',
+]);

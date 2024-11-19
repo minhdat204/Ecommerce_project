69f7c88038bc2fcd1ed89dcb03c4689cf1692053
Dat <0306221011@caothang.edu.vn>
2024-11-20 01:50:07 +0700

    feat(Product): tạo model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table = 'san_pham';
+    protected $primaryKey = 'id_sanpham';
+
+    protected $fillable = [
+        'id_danhmuc',
+        'tensanpham',
+        'slug',
+        'mota',
+        'thongtin_kythuat',
+        'gia',
+        'gia_khuyen_mai',
+        'donvitinh',
+        'xuatxu',
+        'soluong',
+        'trangthai',
+        'luotxem'
+    ];
+
+    public function danhMuc()
+    {
+        return $this->belongsTo(Category::class, 'id_danhmuc', 'id_danhmuc');
+    }
+
+    public function hinhAnh()
+    {
+        return $this->hasMany(ProductImages::class, 'id_sanpham', 'id_sanpham');
+    }
 }

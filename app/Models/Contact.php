a051fa488a2d4425139316f38dc6ff9f84c6f67e
tuhmgg <minhtu15022004@gmail.com>
2024-11-20 09:26:24 +0700

    feat(contact-form): Tao model cho lien_he cho contact

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -1,10 +1,35 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table='lien_he';
+
+    protected $primaryKey='id_lienhe';
+
+    protected $fillable=[
+        'id_nguoidung',
+        'ten',
+        'email',
+        'sodienthoai',
+        'noidung',
+        'trangthai',
+    ];
+    
+    const TrangThai=[
+        'new' => 'new',
+        'processing' => 'processing',
+        'resolved' => 'resolved',
+    ];
+
+    public $timestamps=true;
+    
+    public function nguoidung(){
+        return $this->belongsTo(NguoiDung::class, 'id_nguoidung', 'id_nguoidung'); 
+    }
 }

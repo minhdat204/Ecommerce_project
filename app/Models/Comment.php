312a9a1ee4023a5173b761d0b51316bb3578bc33
Dat <0306221011@caothang.edu.vn>
2024-11-20 13:36:26 +0700

    fix(Comment): sửa model comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $table = 'binh_luan';
+    protected $primaryKey = 'id_binhluan';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_sanpham',
+        'id_nguoidung',
+        'danhgia',
+        'noidung',
+        'parent_id'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'id_sanpham', 'id_sanpham');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_nguoidung', 'id_nguoidung');
+    }
+
+    public function parentComment()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id', 'id_binhluan');
+    }
+
+    public function childComments()
+    {
+        return $this->hasMany(Comment::class, 'parent_id', 'id_binhluan');
+    }
 }

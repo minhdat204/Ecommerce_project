56cee3ec965252d9bbf982330fcc42f6eb820539
yennhi349612 <yeennhi349@gmail.com>
2024-11-20 09:08:30 +0700

    fix(sanphamyeuthich):update model sp yeu thich

--- app/Models/SanPhamYeuThich.php
+++ app/Models/SanPhamYeuThich.php
@@ -1,10 +1,27 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SanPhamYeuThich extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'san_pham_yeu_thich'; 
+
+    protected $fillable = [
+        'id_nguoidung',
+        'id_sanpham',
+        'status', 
+    ];
+    public function sanpham()
+    {
+        return $this->belongsTo(SanPham::class, 'id_sanpham');
+    }
+    
+    public function nguoidung()
+    {
+        return $this->belongsTo(NguoiDung::class, 'id_nguoidung');
+    }
 }

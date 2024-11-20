7dc7e1ff70d542e1f04ee3cceccc95e892f9f617
Dat <0306221011@caothang.edu.vn>
2024-11-20 13:38:50 +0700

    feat(Message): thêm model tin nhắn

---
+++ app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $table = 'tin_nhan';
+    public $timestamps = false;
+    protected $primaryKey = 'id_tinnhan';
+
+    protected $fillable = [
+        'id_phienchat',
+        'id_nguoigui',
+        'noidung',
+        'file_dinh_kem',
+        'thoigian',
+        'trangthai'
+    ];
+
+    public function chatSession()
+    {
+        return $this->belongsTo(ChatSession::class, 'id_phienchat', 'id_phienchat');
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'id_nguoigui', 'id_nguoidung');
+    }
+}

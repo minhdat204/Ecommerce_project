8293047ae9f4fcbb323746a365693ac4e9721548
Dat <0306221011@caothang.edu.vn>
2024-11-23 21:19:51 +0700

    fix(project): revert về ban đầu do merge sai

---
+++ app/Models/Category.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'danh_muc';
+    protected $primaryKey = 'id_danhmuc';
+
+    protected $fillable = [
+        'id_danhmuc_cha',
+        'tendanhmuc',
+        'slug',
+        'mota',
+        'thumbnail',
+        'trangthai'
+    ];
+    // Quan hệ với chính bảng Category
+    public function parentCategory()
+    {
+        return $this->belongsTo(Category::class, 'id_danhmuc_cha', 'id_danhmuc');
+    }
+
+    // Thêm relationship với products
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'id_danhmuc', 'id_danhmuc');
+    }
+
+    // Thêm relationship với child categories
+    public function childCategories()
+    {
+        return $this->hasMany(Category::class, 'id_danhmuc_cha', 'id_danhmuc');
+    }
+}

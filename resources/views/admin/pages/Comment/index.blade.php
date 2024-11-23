111824f8b4880ea5adfa4841ae3cc1abda171ea4
yennhi349612 <yeennhi349@gmail.com>
2024-11-23 12:12:42 +0700

    feat(manage-cmt):cn Xoa cmt trong comment.index

--- resources/views/admin/pages/Comment/index.blade.php
+++ resources/views/admin/pages/Comment/index.blade.php
@@ -1,9 +1,23 @@
 @extends('Admin.Layout.Layout')
 @section('namepage', 'Quản lý Bình Luận')
-
+@push('scripts')
+    <script src="{{ asset('js/delete_comment.js') }}"></script>
+@endpush
 @section('content')
     <div class="container">
         <div class="table-wrapper">
+            @if(session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                <!-- Hiển thị thông báo lỗi -->
+                @if(session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
             <!-- Tiêu đề bảng -->
             <div class="table-title">
                 <div class="row">
@@ -32,12 +46,35 @@
                             <td>{{ $comment->noidung }}</td>
                             <td>{{ $comment->danhgia }}</td>
                             <td>
-                            <a href="#deleteCategoryModal" class="delete" data-toggle="modal">
+                            <a href="#deleteCommentModal" class="delete" data-toggle="modal">
                                     <i class="material-icons" data-toggle="tooltip" title="Xóa">&#xE872;</i>
                                 </a>
                             </td>
                         </tr>
                     @endforeach
+                     <!-- Modal Xóa bình luận -->
+                     <div id="deleteCommentModal" class="modal fade">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <form id="deleteCommentForm" action="" method="POST">
+                                    @csrf
+                                    @method('DELETE') <!-- Phương thức DELETE -->
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">Xóa bình luận</h4>
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Bạn có chắc chắn muốn xóa bình luận này?</p>
+                                        <p class="text-warning"><small>Hành động này không thể hoàn tác.</small></p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <input type="button" class="btn btn-default" data-dismiss="modal" value="Hủy">
+                                        <input type="submit" class="btn btn-danger" value="Xóa">
+                                    </div>
+                                </form>
+                                </div>
+                            </div>
+                        </div>
                 </tbody>
             </table>
 

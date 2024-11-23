00764c3916ff78158e857cf0e0ca72fcc195a2c9
Dat <0306221011@caothang.edu.vn>
2024-11-24 01:22:22 +0700

    fix(Models): Khôi phục contact.php từ nhánh develop

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -1,35 +1,25 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    use HasFactory;
+    protected $table = 'lien_he';
+    protected $primaryKey = 'id_lienhe';
 
-    protected $table='lien_he';
-
-    protected $primaryKey='id_lienhe';
-
-    protected $fillable=[
+    protected $fillable = [
         'id_nguoidung',
         'ten',
         'email',
         'sodienthoai',
         'noidung',
-        'trangthai',
-    ];
-    
-    const TrangThai=[
-        'new' => 'new',
-        'processing' => 'processing',
-        'resolved' => 'resolved',
+        'trangthai'
     ];
 
-    public $timestamps=true;
-    
-    public function nguoidung(){
-        return $this->belongsTo(NguoiDung::class, 'id_nguoidung', 'id_nguoidung'); 
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_nguoidung', 'id_nguoidung');
     }
 }

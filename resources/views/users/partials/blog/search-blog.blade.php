2d8413ad443777ba5c42b506387f93ff0122874d
Dat <0306221011@caothang.edu.vn>
2024-11-24 15:58:34 +0700

    fix

--- resources/views/users/partials/blog/search-blog.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="blog__sidebar__search">
-    <form action="#">
-        <input type="text" placeholder="Search...">
-        <button type="submit"><span class="icon_search"></span></button>
-    </form>
-</div>

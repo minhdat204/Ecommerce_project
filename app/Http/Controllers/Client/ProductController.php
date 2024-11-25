4edee7f4f5210d3ae8754210fa397a0b0c939470
Dat <0306221011@caothang.edu.vn>
2024-11-25 21:56:23 +0700

    fear(ProductController): tạo controller, hàm index và hàm show

---
+++ app/Http/Controllers/Client/ProductController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $ProductsDiscount = Product::whereNotNull('gia_khuyen_mai')
+            ->with('category')
+            ->get();
+        $Products = Product::whereNull('gia_khuyen_mai')->paginate(2);
+        $ProductsCount = Product::whereNull('gia_khuyen_mai')->count();
+        return view('users.pages.shop', compact('Products', 'ProductsDiscount', 'ProductsCount'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $slug)
+    {
+        $sanpham = Product::where('slug', $slug)->fist();
+        return view('users.pages.shop-details', compact('sanpham'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

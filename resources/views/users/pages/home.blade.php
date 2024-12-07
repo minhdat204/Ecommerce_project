378d588ce9fde2c36d416ed514f01a3ab19a323d
Dat <0306221011@caothang.edu.vn>
2024-12-07 16:55:40 +0700

    feat(home): add latest best selling products section to homepage

--- resources/views/users/pages/home.blade.php
+++ resources/views/users/pages/home.blade.php
@@ -13,6 +13,7 @@
     @include('users.partials.home.slide-banner')
     @include('users.partials.home.catagories-section')
     @include('users.partials.home.featured-products')
+    @include('users.partials.home.latest-best_selling')
     @include('users.partials.home.banner')
     @include('users.partials.home.lastest-products')
     @include('users.partials.home.our-services')

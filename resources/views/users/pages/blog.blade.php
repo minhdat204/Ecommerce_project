93ae60ddeceb34225ca175d83667ab182cffd927
yennhi349612 <yeennhi349@gmail.com>
2024-12-14 12:06:13 +0700

    feat(blog/about-us): noi dung

--- resources/views/users/pages/blog.blade.php
+++ resources/views/users/pages/blog.blade.php
@@ -1,20 +1,103 @@
-{{-- users/pages/blog.blade.php --}}
-@extends('layouts.app')
+@extends('users.layouts.layout')
+
+@push('scripts')
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // Khi người dùng submit form tìm kiếm
+        $('#searchForm').on('submit', function(e) {
+            e.preventDefault();  // Ngừng hành động mặc định của form (tải lại trang)
+
+            var keyword = $('#searchInput').val();  // Lấy từ khóa tìm kiếm
+
+            $.ajax({
+                url: '{{ route('blogs.index') }}',  // Địa chỉ gửi yêu cầu đến
+                method: 'GET',
+                data: {
+                    keyword: keyword,  // Gửi từ khóa tìm kiếm
+                    page: 1  // Thêm tham số page để luôn bắt đầu từ trang 1
+                },
+                success: function(response) {
+                    // Cập nhật lại danh sách bài viết
+                    $('#blogItems').html(response.blogs);
+                    // Cập nhật lại phân trang
+                    $('#pagination').html(response.pagination);
+                }
+            });
+        });
+
+        // Xử lý khi nhấn vào phân trang (AJAX pagination)
+        $(document).on('click', '.pagination a', function(e) {
+            e.preventDefault();  // Ngừng hành động mặc định của phân trang
+
+            var page = $(this).attr('href').split('page=')[1];  // Lấy số trang từ URL
+
+            $.ajax({
+                url: '{{ route('blogs.index') }}',
+                method: 'GET',
+                data: {
+                    keyword: $('#searchInput').val(),  // Lấy từ khóa tìm kiếm
+                    page: page  // Gửi số trang
+                },
+                success: function(response) {
+                    // Cập nhật lại danh sách bài viết
+                    $('#blogItems').html(response.blogs);
+                    // Cập nhật lại phân trang
+                    $('#pagination').html(response.pagination);
+                }
+            });
+        });
+    });
+</script>
+@endpush
 
 @section('content')
+<!-- Blog Section Begin -->
+<section class="blog spad">
     <div class="container">
-        <h1>Blog</h1>
-        
-        {{-- Hiển thị danh sách bài viết --}}
         <div class="row">
-            @foreach ($posts as $post)
-                @include('users.partials.blog.blog-item', ['post' => $post])
-            @endforeach
-        </div>
+            <div class="col-lg-4 col-md-5">
+                <div class="blog__sidebar">
+                    <!-- Thanh tìm kiếm -->
+                    <div class="blog__sidebar__item">
+                        @include('users.partials.blog.search-blog')
+                    </div>
 
-        {{-- Hiển thị phân trang --}}
-        <div class="d-flex justify-content-center">
-            {{ $posts->links('pagination::bootstrap-4') }}
+                    <!-- Bài viết gần đây -->
+                    <div class="blog__sidebar__item">
+                        @include('users.partials.blog.recent-news')
+                    </div>
+
+                    <!-- Thẻ tìm kiếm -->
+                    <div class="blog__sidebar__item">
+                        <h4>Search By</h4>
+                        <div class="blog__sidebar__item__tags">
+                            <a href="#">Apple</a>
+                            <a href="#">Beauty</a>
+                            <a href="#">Vegetables</a>
+                            <a href="#">Fruit</a>
+                            <a href="#">Healthy Food</a>
+                            <a href="#">Lifestyle</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Danh sách bài viết -->
+            <div class="col-lg-8 col-md-7">
+                <div class="row">
+                    @include('users.partials.blog.blog-about-us')
+                    @include('users.partials.blog.blog-item')
+                    <!-- Phân trang -->
+                <div class="col-lg-12 pagination-container">
+                    <div class="product__pagination blog__pagination" id="pagination">
+                            {{ $posts->links('pagination::bootstrap-4') }}
+                    </div>
+                </div>
+                </div>
+            </div>
         </div>
     </div>
+</section>
+<!-- Blog Section End -->
 @endsection

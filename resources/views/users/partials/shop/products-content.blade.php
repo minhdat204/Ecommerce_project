8d848273f7b4007526677102638a8b71301bd625
Dat <0306221011@caothang.edu.vn>
2024-12-21 17:09:01 +0700

    fix(shop): cập nhật tên biến để đảm bảo tính nhất quán và cải thiện logic phân trang đồng thời cải tiến việc hiển thị tất cả sản phẩm thay vì 1 số sản phẩm không giảm giá

--- resources/views/users/partials/shop/products-content.blade.php
+++ resources/views/users/partials/shop/products-content.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="col-lg-4 col-md-4">
             <div class="filter__found">
-                <h6><span>{{$ProductsCount}}</span> Products found</h6>
+                <h6><span>{{$productsCount}}</span> Products found</h6>
             </div>
         </div>
         <div class="col-lg-4 col-md-3">
@@ -23,7 +23,7 @@
     </div>
 </div>
 <div class="row">
-    @foreach ($Products as $SanPham)
+    @foreach ($products as $sanpham)
     <div class="col-lg-4 col-md-6 col-sm-6">
         <div class="product__item">
             <div class="product__item__pic set-bg" data-setbg="img/product/product-1.jpg">
@@ -34,9 +34,16 @@
                 </ul>
             </div>
             <div class="product__item__text">
-                <h6><a href="{{route('users.shop_details', $SanPham->slug)}}">{{$SanPham->tensanpham}}</a></h6>
+                <h6><a href="{{route('users.shop_details', $sanpham->slug)}}">{{$sanpham->tensanpham}}</a></h6>
                 <h5>
-                    <span>${{$SanPham->gia}}</span>
+                    <div class="product__item__price">
+                        @if ($sanpham->gia_khuyen_mai != 0)
+                            ${{$sanpham->gia_khuyen_mai}}
+                            <span>${{$sanpham->gia}}</span>
+                        @else
+                            ${{$sanpham->gia}}
+                        @endif
+                    </div>
                 </h5>
             </div>
         </div>
@@ -44,17 +51,17 @@
     @endforeach
 </div>
 <div class="product__pagination">
-    @if ($Products->lastPage() > 1)
-        @if ($Products->currentPage() > 1)
-            <a href="{{ $Products->url($Products->currentPage() - 1) }}"><i class="fa fa-long-arrow-left"></i></a>
+    @if ($products->lastPage() > 1)
+        @if ($products->currentPage() > 1)
+            <a href="{{ $products->url($products->currentPage() - 1) }}"><i class="fa fa-long-arrow-left"></i></a>
         @endif
 
-        @for ($i = 1; $i <= $Products->lastPage(); $i++)
-            <a href="{{ $Products->url($i) }}" class="{{ ($Products->currentPage() == $i) ? 'active' : '' }}">{{ $i }}</a>
+        @for ($i = 1; $i <= $products->lastPage(); $i++)
+            <a href="{{ $products->url($i) }}" class="{{ ($products->currentPage() == $i) ? 'active' : '' }}">{{ $i }}</a>
         @endfor
 
-        @if ($Products->currentPage() < $Products->lastPage())
-            <a href="{{ $Products->url($Products->currentPage() + 1) }}"><i class="fa fa-long-arrow-right"></i></a>
+        @if ($products->currentPage() < $products->lastPage())
+            <a href="{{ $products->url($products->currentPage() + 1) }}"><i class="fa fa-long-arrow-right"></i></a>
         @endif
     @endif
 </div>

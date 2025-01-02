4130d99d01a5fbdba26cae8819d8b1230a24c514
yennhi349612 <yeennhi349@gmail.com>
2025-01-02 18:52:45 +0700

    feat(shop-product.discount): icons addToCart

--- resources/views/users/partials/shop/products-discount.blade.php
+++ resources/views/users/partials/shop/products-discount.blade.php
@@ -11,7 +11,10 @@
                     <ul class="product__item__pic__hover">
                         <li><a href="#"><i class="fa fa-heart"></i></a></li>
                         <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                        <li>
+                            <a href="#" onclick="addToCart(this)" class="add-to-cart" data-id="{{ $sanpham->id_sanpham }}" data-quantity="1">
+                                <i class="fa fa-shopping-cart"></i>
+                            </a>
                     </ul>
                 </div>
                 <div class="product__discount__item__text">

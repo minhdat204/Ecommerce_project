3bc25ddf75666d02584e6e9736ceb2a3b697f6aa
Dat <0306221011@caothang.edu.vn>
2025-01-04 00:52:49 +0700

    feat(Shop): update price range slider min/max values and make input fields readonly

--- resources/views/users/partials/shop/price-filter.blade.php
+++ resources/views/users/partials/shop/price-filter.blade.php
@@ -1,15 +1,15 @@
 <h4>Price</h4>
 <div class="price-range-wrap">
     <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
-        data-min="1" data-max="10000">
+        data-min="1000" data-max="1000000">
         <div class="ui-slider-range ui-corner-all ui-widget-header"></div>
         <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
         <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
     </div>
     <div class="range-slider">
         <div class="price-input">
-            <input type="text" id="minamount">
-            <input type="text" id="maxamount">
+            <input type="text" id="minamount" readonly>
+            <input type="text" id="maxamount" readonly>
         </div>
     </div>
 </div>

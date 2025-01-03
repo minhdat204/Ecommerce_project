9b9484728d0627ee66323f696beb86d5790d84da
Dat <0306221011@caothang.edu.vn>
2025-01-03 23:37:38 +0700

    feat(Shop): update price filter range to allow a minimum of 1 and a maximum of 10000

--- resources/views/users/partials/shop/price-filter.blade.php
+++ resources/views/users/partials/shop/price-filter.blade.php
@@ -1,7 +1,7 @@
 <h4>Price</h4>
 <div class="price-range-wrap">
     <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
-        data-min="10" data-max="540">
+        data-min="1" data-max="10000">
         <div class="ui-slider-range ui-corner-all ui-widget-header"></div>
         <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
         <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>

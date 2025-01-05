f02039725c32c27ee0884c7ce010f939cb6cf3bd
Dat <0306221011@caothang.edu.vn>
2025-01-06 03:31:06 +0700

    feat(shop): implement quick add to cart functionality for product items

--- resources/views/users/pages/shop-details.blade.php
+++ resources/views/users/pages/shop-details.blade.php
@@ -181,7 +181,7 @@
                                 <ul class="product__item__pic__hover">
                                     <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                     <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                                    <li><a href="javascript:void(0)" onclick="quickAddToCart({{ $product->id_sanpham }})"><i class="fa fa-shopping-cart"></i></a></li>
                                 </ul>
                             </div>
                             <div class="product__item__text">
--- resources/views/users/partials/shop/products-discount.blade.php
+++ resources/views/users/partials/shop/products-discount.blade.php
@@ -11,7 +11,7 @@
                     <ul class="product__item__pic__hover">
                         <li><a href="#"><i class="fa fa-heart"></i></a></li>
                         <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                        <li><a href="javascript:void(0)" onclick="quickAddToCart({{ $product->id_sanpham }})"><i class="fa fa-shopping-cart"></i></a></li>
                     </ul>
                 </div>
                 <div class="product__discount__item__text">
